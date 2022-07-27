929d1c73fc320c1328707cea169318afc3b98c0e
Aiano_czm <Aiano_czm@163.com>
2022-07-28 01:04:16 +0800

    first commit

---
+++ car_leader/Debug/bsp/subdir_vars.mk
@@ -0,0 +1,26 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../bsp/bsp_can.c 
+
+C_DEPS += \
+./bsp/bsp_can.d 
+
+OBJS += \
+./bsp/bsp_can.obj 
+
+OBJS__QUOTED += \
+"bsp\bsp_can.obj" 
+
+C_DEPS__QUOTED += \
+"bsp\bsp_can.d" 
+
+C_SRCS__QUOTED += \
+"../bsp/bsp_can.c" 
+
+
